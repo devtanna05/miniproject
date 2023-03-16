0142cd40ccf2dafbbaca724cd6b532926a5a680f
devtanna05 <93508859+devtanna05@users.noreply.github.com>
2023-03-16 09:28:58 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var loader = document.getElementById("loading");
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+})
+
+// const form = document.getElementById('form');
+// const firstname = document.getElementById('firstname');
+// const lastname = document.getElementById('lastname');
+
+
+// form.addEventListener('submit', function (e) {
+//   e.preventDefault();
+
+//   const firstnameValue = firstname.value;
+//   const lastnameValue = lastname.value;
+
+
+//   localStorage.setItem('first-name', firstnameValue);
+//   localStorage.setItem('last-name', lastnameValue);
+
+//   window.location.href = "booking.html";
+// })
+
+
+
+// const firstName = localStorage.getItem('first-name');
+// const lastName = localStorage.getItem('last-name');
+
+// document.getElementById('first-name').textContent = firstName;
+// // document.getElementsByClassName('first-name').textContent = firstName;
+// document.getElementById('last-name').textContent = lastName;
+//     // document.getElementsByClassName('last-name').textContent = lastName;
